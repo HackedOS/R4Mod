35a68bef961ac7d1f6dfa31dabba2b649aae8cb3
HackedOS <gmachina43@gmail.com>
2022-10-31 22:09:28 +0530

    fix previous commit

--- src/main/java/xyz/hackos/r4tech/r4mod/others/Config.java
+++ src/main/java/xyz/hackos/r4tech/r4mod/others/Config.java
@@ -1,6 +1,44 @@
 package xyz.hackos.r4tech.r4mod.others;
 
-public record Config(String botToken, String chatChannelID, String serverStartingPrompt, String serverStartedPrompt,
-                     String serverStoppingPrompt, String serverStoppedPrompt) {
+public class Config {
+    private String botToken;
+    private String chatChannelID;
+    private String serverStartingPrompt;
+    private String serverStartedPrompt;
+    private String serverStoppingPrompt;
+    private String serverStoppedPrompt;
 
+    public Config(String botToken, String chatChannelID, String serverStartingPrompt, String serverStartedPrompt,
+                  String serverStoppingPrompt, String serverStoppedPrompt) {
+        this.botToken = botToken;
+        this.chatChannelID = chatChannelID;
+        this.serverStartingPrompt = serverStartingPrompt;
+        this.serverStartedPrompt = serverStartedPrompt;
+        this.serverStoppingPrompt = serverStoppingPrompt;
+        this.serverStoppedPrompt = serverStoppedPrompt;
+    }
+
+    public String botToken() {
+        return botToken;
+    }
+
+    public String chatChannelID() {
+        return chatChannelID;
+    }
+
+    public String serverStartingPrompt() {
+        return serverStartingPrompt;
+    }
+
+    public String serverStartedPrompt() {
+        return serverStartedPrompt;
+    }
+
+    public String serverStoppingPrompt() {
+        return serverStoppingPrompt;
+    }
+
+    public String serverStoppedPrompt() {
+        return serverStoppedPrompt;
+    }
 }
